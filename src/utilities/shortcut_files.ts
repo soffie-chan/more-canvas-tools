961d3161e79a7a894a8baec237e61d111c9dfd80
Soffie <soffie@udel.edu>
2026-03-19 20:57:25 -0400

    editfile and deletefile buttons now work :) should be good to go overall!

--- src/utilities/shortcut_files.ts
+++ src/utilities/shortcut_files.ts
@@ -74,60 +74,52 @@ export function createFile(file: fileStore){
             const shortcuts = JSON.parse(localStorage.getItem("files") || "[]") as fileStore[];
             const updated = shortcuts.filter(file => "🗁 "+ file.fileName+ "✎X" !== fileName.textContent );
             localStorage.setItem("files", JSON.stringify(updated));
-        console.log("🗁 "+ file.fileName+"✎X")
-        console.log(fileName.textContent )
         loadShortcuts();
         loadFiles();
     });
 
-    // editShortcut.addEventListener("click", (event)=>{
-    //     if (document.getElementById("edit_shortcut_menu")) return //prevent spam
-    //     const EditMenuContent = document.createElement("div");
-    //     EditMenuContent.id = "edit_shortcut_menu"
-    //     document.body.appendChild(EditMenuContent)
+    editFile.addEventListener("click", (event)=>{
+        event.preventDefault();
+        event.stopPropagation();
+        if (document.getElementById("edit_file_menu")) return //prevent spam
+        const EditFileMenuContent = document.createElement("div");
+        EditFileMenuContent.id = "edit_shortcut_menu"
+        document.body.appendChild(EditFileMenuContent)
 
-    //     EditMenuContent.innerHTML = `
-    //         <div id="edit_shortcut_menu">
-    //             <div style="padding: 10px;">
-    //                 <strong>Edit Shortcut</strong>
-    //             </div>
-    //         <form>
-    //             <label for="edit_sc_name">Shortcut Name</label>
-    //             <input type="text" id="edit_sc_name" name="Edit_Shortcut_Name"><br><br>
-    //             <label for="edit_sc_url">Shortcut URL</label>
-    //             <input type="url" id="edit_sc_url" name="Edit_Shortcut_URL"><br><br>
-    //         </form>
-    //         <button id=save_edit>Save</button>
-    //         <button id=cancel_edit>Cancel</button>
-    //         </div>
-    //     </div>
-    //     `;
-    //     const editNameBar = EditMenuContent.querySelector<HTMLInputElement>("#edit_sc_name")!;;
-    //     editNameBar.value = name;
-    //     const editURLBar = EditMenuContent.querySelector<HTMLInputElement>("#edit_sc_url")!;
-    //     editURLBar.value = url;
+        EditFileMenuContent.innerHTML = `
+            <div id="edit_file_menu">
+                <div style="padding: 10px;">
+                    <strong>Edit File</strong>
+                </div>
+            <form>
+                <label for="edit_file_name">File Name</label>
+                <input type="text" id="editfilename" name="Edit_File_Name"><br><br>
+            </form>
+            <button id=save_file_edit>Save</button>
+            <button id=cancel_file_edit>Cancel</button>
+            </div>
+        </div>
+        `;
 
-    //     const SaveEditButton = EditMenuContent.querySelector<HTMLButtonElement>("#save_edit")!;
-    //     SaveEditButton.addEventListener("click", (e)=>{
-    //         if (!editNameBar.value || !editURLBar.value) return;
-    //         if (fileIndex !== undefined) {
-    //             const files = JSON.parse(localStorage.getItem("files") || "[]") as fileStore[];
-    //             files[fileIndex].fileContents = files[fileIndex].fileContents.map(l => l.id === id ? {name: editNameBar.value, url: editURLBar.value, id} : l);
-    //             localStorage.setItem("files", JSON.stringify(files));
-    //         } else {
-    //             const shortcuts = JSON.parse(localStorage.getItem("shortcuts") || "[]") as { name:string; url:string; id: string }[];
-    //             const updated = shortcuts.map(shortcut => shortcut.id === id ? {name:editNameBar.value, url:editURLBar.value, id} : shortcut);
-    //             localStorage.setItem("shortcuts", JSON.stringify(updated));
-    //         }
-    //         loadShortcuts();
-    //         loadFiles();
-    //         EditMenuContent.remove();
+        const editFileNameBar = EditFileMenuContent.querySelector<HTMLInputElement>("#editfilename")!;
+        editFileNameBar.value = file.fileName;
 
-    //     });
-    //     const CancelEditButton = EditMenuContent.querySelector<HTMLButtonElement>("#cancel_edit")!;
-    //     CancelEditButton.addEventListener("click", (e)=>{
-    //         EditMenuContent.remove();
-    //     });
+        const SaveFileEditButton = EditFileMenuContent.querySelector<HTMLButtonElement>("#save_file_edit")!;
+        SaveFileEditButton.addEventListener("click", (e)=>{
+            if (!editFileNameBar.value) return;
+            const files = JSON.parse(localStorage.getItem("files") || "[]") as fileStore[];
+            const updated = files.map(file =>  "🗁 "+ file.fileName+ "✎X" === fileName.textContent ? {...file, fileName: editFileNameBar.value} : file);
+            localStorage.setItem("files", JSON.stringify(updated));
+            loadShortcuts();
+            loadFiles();
+            EditFileMenuContent.remove();
+        });
+
+        const CancelEditButton = EditFileMenuContent.querySelector<HTMLButtonElement>("#cancel_file_edit")!;
+        CancelEditButton.addEventListener("click", (e)=>{
+            EditFileMenuContent.remove();
+        });
+    });
 
 
     fileName.appendChild(editFile);
